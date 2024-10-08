eea6bf9b30c2735c7786728ee85ad3c325498c01
Minhaz <sharifmdminhazurrahman@gmail.com>
2024-10-08 16:05:34 +0600

    feat: add hero section

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/sections/Header";
+import Hero from "./components/sections/Hero";
+
+function App() {
+	return (
+		<main className="overflow-hidden">
+			<Header />
+			<Hero />
+		</main>
+	);
+}
+
+export default App;
