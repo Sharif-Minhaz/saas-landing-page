34b303c89512155023deec5bd2befbff35fba4ed
Minhaz <sharifmdminhazurrahman@gmail.com>
2024-10-08 19:22:17 +0600

    feat: add features section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Features from "./components/sections/Features";
 import Header from "./components/sections/Header";
 import Hero from "./components/sections/Hero";
 
@@ -7,6 +8,7 @@ function App() {
 		<main className="overflow-hidden">
 			<Header />
 			<Hero />
+			<Features />
 		</main>
 	);
 }
--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -1,9 +1,22 @@
 import clsx from "clsx";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link as LinkScroll } from "react-scroll";
 
 export default function Header() {
 	const [isOpen, setIsOpen] = useState(false);
+	const [hasScrolled, setHasScrolled] = useState(false);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			setHasScrolled(window.scrollY > 32);
+		};
+
+		window.addEventListener("scroll", handleScroll);
+
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
 
 	const NavLink = ({ title }) => (
 		<LinkScroll className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5">
@@ -12,7 +25,12 @@ export default function Header() {
 	);
 
 	return (
-		<header className="fixed top-0 left-0 z-50 w-full py-10">
+		<header
+			className={clsx(
+				"fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4",
+				hasScrolled && "py-2 bg-black-100 backdrop-blur-[8px]"
+			)}
+		>
 			<div className="container flex h-14 items-center max-lg:px-5">
 				<a href="" className="lg:hidden flex-1 cursor-pointer z-2">
 					<img src="/images/xora.svg" alt="xora" width={115} height={55} />
